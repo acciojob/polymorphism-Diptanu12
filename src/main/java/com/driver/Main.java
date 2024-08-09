5adceb9aeee9a18a587555bc0a26d86a4095b340
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-08-09 21:07:15 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,3 +1,4 @@
+
 package com.driver;
 
 class Main {
@@ -5,9 +6,11 @@ class Main {
         public int product(int x, int y) {
             return x * y;
         }
+
         public int product(int x, int y, int z) {
             return x * y * z;
         }
+
         public double product(double x, double y) {
             return x * y;
         }
