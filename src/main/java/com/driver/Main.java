73df12ae3d7da47193c22731d67f87d718a21845
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-08-09 21:23:23 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 class Main {
-    public class Product {
+    public static class Product { 
         public int product(int x, int y) {
             return x * y;
         }
@@ -16,7 +16,6 @@ class Main {
     }
 
     public static void main(String[] args) {
-        Product p = new Product();
-        
+        Product p = new Product(); // Now this works fine
     }
 }
